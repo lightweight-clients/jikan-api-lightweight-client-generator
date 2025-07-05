65db5ce5e7c527e48646c1c097d80924ca6e7a72
HRAshton <12210721+HRAshton@users.noreply.github.com>
2025-07-05 18:50:24 +0200

    Rename types modules, fix parameterless requests methods

--- src/generator.ts
+++ src/generator.ts
@@ -1,4 +1,4 @@
-﻿import type { OpenApi } from './types';
+﻿import type { OpenApi, Parameter } from './types';
 import * as config from './config.json';
 import * as fs from 'node:fs';
 import * as operationId from 'node:path';
@@ -11,7 +11,9 @@ interface EndpointRenderData {
   url: string;
   description: string;
   baseEndpointName: string;
-  hasParameters: boolean;
+  pathParameters: Parameter[];
+  hasQueryParameters: boolean;
+  hasDataInResponse: boolean;
 }
 
 interface ClientRenderData {
@@ -29,43 +31,48 @@ const fetchSpecification = async (): Promise<OpenApi> => {
   return openApi;
 };
 
-const addUrlParameters = (url: string): string => {
-  return url.replaceAll(/{([^}]+)}/g, (_, arg) => '${args.' + arg + '}');
-};
-
 const getClientRenderData = (openApi: OpenApi): ClientRenderData => {
   const endpoints: EndpointRenderData[] = [];
   for (const [url, methods] of Object.entries(openApi.paths)) {
-    assert(methods, `Path should have methods defined`);
+    assert(methods, 'Path should have methods defined');
     assert(
       Object.keys(methods).length === 1,
-      `Path should have exactly one method (GET)`,
+      'Path should have exactly one method (GET)',
     );
-    assert(methods.get, `Path should have a GET method`);
-    assert(methods.get.operationId, `Path should have an operationId`);
+    assert(methods.get, 'Path should have a GET method');
+    assert(methods.get.operationId, 'Path should have an operationId');
     assert(
       /^[a-zA-Z0-9_]+$/.test(methods.get.operationId),
       `Path ${methods.get.operationId} should contain only letters, numbers, and underscores`,
     );
     assert(
       methods.get.responses['200'],
-      `Path should have a 200 response defined`,
+      'Path should have a 200 response defined',
     );
     assert(
       methods.get.responses['200'].description,
-      `Path should have a description for the 200 response`,
+      'Path should have a description for the 200 response',
     );
 
-    const { operationId, responses } = methods.get;
+    const { operationId, parameters, responses } = methods.get;
+
+    const pathParameters = parameters?.filter((p) => p.in === 'path') || [];
+    assert(
+      pathParameters.length === 0 || pathParameters[0].required,
+      'Path parameters should be required',
+    );
 
     const endpoint: EndpointRenderData = {
       name: operationId,
-      url: addUrlParameters(url.slice(1)),
+      url: url.slice(1),
       description: responses['200'].description,
       baseEndpointName:
         operationId.charAt(0).toUpperCase() + operationId.slice(1),
-      hasParameters:
-        !!methods.get.parameters && methods.get.parameters.length > 0,
+      hasQueryParameters: parameters?.some((p) => p.in === 'query') || false,
+      hasDataInResponse:
+        Object.keys(responses['200'].content['application/json'].schema)
+          .length > 0,
+      pathParameters,
     };
     endpoints.push(endpoint);
   }
@@ -123,8 +130,8 @@ export const generate = async () => {
   await generateEtaFile('core', {});
   await generateEtaFile('index', {});
   await generateEtaFile('client', renderData);
-  await generateEtaFile('simple-types', renderData);
-  await generateTypesFile(openApi, `${config.output_folder}/types.ts`);
+  await generateEtaFile('types', renderData);
+  await generateTypesFile(openApi, `${config.output_folder}/raw-types.ts`);
 };
 
 generate()
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,28 @@
-﻿export interface EndpointData {
+﻿export type Parameter = {
+  name: string;
+  in: 'path' | 'query' | 'header' | 'cookie';
+  required: boolean;
+  schema: {
+    type: 'string' | 'number' | 'boolean' | 'integer';
+  };
+};
+
+export type EndpointData = {
   get: {
     operationId: string;
-    parameters?: [];
+    parameters?: Parameter[];
     responses: {
       [statusCode: string]: {
         description: string;
+        content: {
+          'application/json': {
+            schema: Record<string, unknown>;
+          };
+        };
       };
     };
   };
-}
+};
 
 export type OpenApi = Record<string, unknown> & {
   paths: {
